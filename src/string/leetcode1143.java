082c4b1053e5ef95c8b06dfdcf6339f8220990ee
Administrator <15857186703@163.com>
2020-12-09 18:58:34 +0800

    test

--- src/string/leetcode1143.java
+++ src/string/leetcode1143.java
@@ -6,19 +6,25 @@ class leetcode1143 {
         int m=text1.length();
         int n=text2.length();
         int dp[][]=new int[m+1][n+1];
+        StringBuilder common=new StringBuilder();
         for(int i=1;i<=m;i++)
             for(int j=1;j<=n;j++){
-                if(text1.charAt(i-1)==text2.charAt(j-1))
+                if(text1.charAt(i-1)==text2.charAt(j-1)){
                     dp[i][j]=dp[i-1][j-1]+1;
+                    if(dp[i][j]!=dp[i-1][j] && dp[i][j]!=dp[i][j-1])
+                        common.append(text1.charAt(i-1));
+                    }
                 else
+
                     dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
             }
+        System.out.println("最长公共子序列为："+common.toString());
         return dp[m][n];
     }
 
     public static void main(String[] args) {
-        String s1="hasswefasdeq";
+        String s1="haswfasdeq";
         String s2="aswde";
-        System.out.println(longestCommonSubsequence(s1, s2));
+        System.out.println("最长公共子序列长度："+longestCommonSubsequence(s1, s2));
     }
 }
