581359e82d08a2be40a75e22a79bc218acd645ee
Administrator <15857186703@163.com>
2020-12-09 19:34:08 +0800

    test

--- src/string/leetcode1143.java
+++ src/string/leetcode1143.java
@@ -6,25 +6,43 @@ class leetcode1143 {
         int m=text1.length();
         int n=text2.length();
         int dp[][]=new int[m+1][n+1];
+        int flag[][]=new int[m+1][n+1];
         StringBuilder common=new StringBuilder();
         for(int i=1;i<=m;i++)
             for(int j=1;j<=n;j++){
                 if(text1.charAt(i-1)==text2.charAt(j-1)){
                     dp[i][j]=dp[i-1][j-1]+1;
-                    if(dp[i][j]!=dp[i-1][j] && dp[i][j]!=dp[i][j-1])
-                        common.append(text1.charAt(i-1));
+                    flag[i][j]=0;
                     }
-                else
+                else if(dp[i-1][j]>dp[i][j-1]){
+                    dp[i][j]=dp[i-1][j];
+                    flag[i][j]=1;
+                }
 
-                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                else {
+                    dp[i][j]=dp[i][j-1];
+                    flag[i][j]=2;
+                }
             }
-        System.out.println("最长公共子序列为："+common.toString());
+        System.out.println("最长公共子序列为："+createStr(flag,m,n,text1));
         return dp[m][n];
     }
 
+    public static String createStr(int[][] flag,int i,int j,String text1 ){
+        if(i==0||j==0)
+            return "";
+        if(flag[i][j]==0){
+            return createStr(flag,i-1,j-1,text1)+text1.charAt(i-1);
+        }
+        else if(flag[i][j]==1)
+            return createStr(flag,i-1,j,text1);
+        else
+            return createStr(flag,i,j-1,text1);
+    }
+
     public static void main(String[] args) {
-        String s1="haswfasdeq";
-        String s2="aswde";
+        String s1="accggtggagtgcgcggaagccggccgaa";
+        String s2="gtcgttcggaatgccgttgctctgtaaa";
         System.out.println("最长公共子序列长度："+longestCommonSubsequence(s1, s2));
     }
 }
