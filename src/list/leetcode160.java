2dfefb2061dc5461c2f203410c68cb1cff9eab3b
Administrator <15857186703@163.com>
2020-12-10 18:42:24 +0800

    test

---
+++ src/list/leetcode160.java
@@ -0,0 +1,16 @@
+package list;
+
+public class leetcode160 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+        ListNode l1=headA;
+        ListNode l2=headB;
+        if(headA==null||headB==null)
+            return null;
+        while(l1!=l2){
+            l1=l1==null?headB:l1.next;
+            l2=l2==null?headA:l2.next;
+        }
+        return l1;
+    }
+}
