2d73f1266f3f2215cbe9415fc923e4701ec965de
SilenceZ <15857186703@163.com>
2021-03-30 10:23:18 +0800

    test

---
+++ src/华为机试题/HJ7.java
@@ -0,0 +1,11 @@
+package 华为机试题;
+
+import java.util.Scanner;
+
+public class HJ7 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        float s=scanner.nextFloat();
+
+    }
+}
