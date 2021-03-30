a2412b0fbe2231d80da11d284e36019c8b40e499
SilenceZ <15857186703@163.com>
2021-03-30 10:37:46 +0800

    test

--- src/华为机试题/HJ7.java
+++ src/华为机试题/HJ7.java
@@ -1,11 +1,13 @@
 package 华为机试题;
 
+//输入一个数的近似值，按四舍五入
 import java.util.Scanner;
 
 public class HJ7 {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
-        float s=scanner.nextFloat();
-
+        double s=scanner.nextDouble();
+        int res= (int) Math.floor(s+0.5);
+        System.out.println(res);
     }
 }
---
+++ src/华为机试题/HJ8.java
@@ -0,0 +1,13 @@
+package 华为机试题;
+
+import java.util.Scanner;
+
+//数据表记录包含表索引和数值（int范围的正整数），请对表索引相同的记录进行合并，
+// 即将相同索引的数值进行求和运算，输出按照key值升序进行输出。
+public class HJ8 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+
+    }
+}
