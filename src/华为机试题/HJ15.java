a2ac9cc5541df607cf57e010031cdc3ae7f1a575
SilenceZ <15857186703@163.com>
2021-04-02 23:56:31 +0800

    test1

---
+++ src/华为机试题/HJ15.java
@@ -0,0 +1,18 @@
+package 华为机试题;
+
+import java.util.Scanner;
+
+//输入一个int型的正整数，计算出该int型数据在内存中存储时1的个数
+public class HJ15 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int count = 0;
+        while(n != 0) {
+            count ++;
+            n = n & (n - 1);
+        }
+        System.out.println(count);
+        scanner.close();
+    }
+}
